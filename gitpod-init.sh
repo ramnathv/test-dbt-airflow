49aba2a0a0fc174558676cb8673a821d14d18baa
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-02-23 21:08:44 +0000

    feat: install airflow

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -2,3 +2,16 @@ echo "Running the init script"
 
 psql -f /dbt/scripts/init.sql
 psql -d dbt -f /dbt/scripts/schema.sql
+
+export AIRFLOW_HOME=~/airflow
+
+# Install Airflow using the constraints file
+AIRFLOW_VERSION=2.2.4
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+# For example: 3.6
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.2.2/constraints-3.6.txt
+# echo "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install --upgrade pip
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}" --user
+pip install dag-factory
