195df86b6438110f01f3c80c66c83ef105bb6d1d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-02-28 19:06:55 +0000

    feat: update gitpod shell files

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -3,3 +3,7 @@ mkdir -p /workspace/.dbt
 ln -snf /workspace/.dbt ~/.dbt
 
 export PGPASSWORD=gitpod PGDATABASE=dbt
+export AIRFLOW__CORE__DAGS_FOLDER=/workspace/test-dbt-airflow/airflow/dags
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
+# airflow standalone
+
--- gitpod-init.sh
+++ gitpod-init.sh
@@ -14,4 +14,20 @@ CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${A
 # echo "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 pip install --upgrade pip
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}" --user
-pip install dag-factory
+pip install dag-factory airflow-dbt pandas
+
+# Install R and dependencies
+sudo apt update -qq -y
+# install two helper packages we need
+sudo apt install --no-install-recommends software-properties-common dirmngr -y
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+# Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo apt install --no-install-recommends r-base -y
+sudo apt install r-base-dev -y
+sudo apt install libcurl4-openssl-dev libssl-dev libxml2-dev -y
+sudo apt install r-cran-tidyverse r-cran-rmarkdown r-cran-devtools -y
